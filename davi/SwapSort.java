f53cd1a49f963ff0d44818d6254fc2523804ef3f
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2025-12-03 18:55:26 -0300

    Anotação SwapSort

---
+++ davi/SwapSort.java
@@ -0,0 +1,49 @@
+public class SwapSort {
+
+    /*@ public normal_behavior
+      @   requires array != null;
+      @   requires (\forall int i, j; 0 <= i && i < j && j < array.length; array[i] != array[j]);
+      @   assignable array[*];
+      @*/
+    public void sort(int[] array) {
+        int index = 0;
+
+        if (array.length == 0) return;
+
+        //@ loop_invariant 0 <= index < array.length;
+        while (index < array.length - 1) {
+            int amountSmallerElements = getSmallerElementCount(array, index);
+
+            if (amountSmallerElements > 0) {
+                int target = index + amountSmallerElements;
+                //@ assert target < array.length;
+
+                int temp = array[index];
+                array[index] = array[target];
+                array[target] = temp;
+            } else {
+                index++;
+            }
+        }
+    }
+
+    /*@ private normal_behavior
+      @   requires array != null;
+      @   requires 0 <= index < array.length;
+      @   ensures 0 <= \result <= (array.length - 1 - index);
+      @*/
+    private /*@ pure helper @*/ int getSmallerElementCount(int[] array, int index) {
+        int counter = 0;
+        
+        /*@ loop_invariant index + 1 <= i <= array.length;
+          @ loop_invariant 0 <= counter <= (i - (index + 1));
+          @ decreases array.length - i;
+          @*/
+        for (int i = index + 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
